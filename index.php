8d143caa5bcf105f7cec15e8a4555946c26898e7
msantiagoc33 <msantiagoc33@192.168.1.36>
2018-02-17 20:51:20 +0100

    MyWeb: Segunda version. Padding superior a 100px.

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Mi web 2018</title>
         <link rel="stylesheet" href="style.css?v=1" type="text/css" media="all" />
     </head>
     <body>
-        <div id="header"><?php echo '<h1>Bienvenido a mi Web</h1>';?></div>           
+        <div id="header"><?php echo '<h1>Bienvenido a mi Web</h1>';?>
+            <p> Lorem ipsum dolor sit </p>
+        </div>           
     </body>
 </html>
