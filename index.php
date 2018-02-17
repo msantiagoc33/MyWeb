1f1b0de66abeacfa3014d7ec04c6802b3a1a1ded
msantiagoc33 <msantiagoc33@192.168.1.36>
2018-02-17 20:37:51 +0100

    MyWeb: Primera version

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="style.css?v=1" type="text/css" media="all" />
+    </head>
+    <body>
+        <div id="header"><?php echo '<h1>Bienvenido a mi Web</h1>';?></div>           
+    </body>
+</html>
