74e65768a17615e5a98f4a30d4adf7b06757c50c
msantiagoc33 <msantiagoc33@192.168.1.36>
2018-02-19 10:37:26 +0100

    MyWeb: Tercera version

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
         <link rel="stylesheet" href="style.css?v=1" type="text/css" media="all" />
     </head>
     <body>
-        <div id="header"><?php echo '<h1>Bienvenido a mi Web</h1>';?>
-            <p> Lorem ipsum dolor sit </p>
+        <div id="header"><?php echo '<h1>Bienvenido a mi Web de prueba</h1>';?>
+            <p> Lorem ipsum <strong>dolor</strong> sit </p>
         </div>           
     </body>
 </html>
